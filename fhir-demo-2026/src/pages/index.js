9fb1ce97f66b89cdb0828c3d0c7777d3b68f6ef0
Mikael Rinnetmäki <mikael@sensotrend.com>
2026-02-28 16:06:04 +0200

    Add hackathon contents

--- fhir-demo-2026/src/pages/index.js
+++ fhir-demo-2026/src/pages/index.js
@@ -1,6 +1,10 @@
 import * as React from "react"
 import { Link } from 'gatsby';
 
+import hl7DenmarkLogo from '../images/hl7-denmark.png';
+import hl7FinlandLogo from '../images/hl7-finland.svg';
+import hl7NorwayLogo from '../images/hl7-norway.png';
+import hl7SwedenLogo from '../images/hl7-sweden.png';
 import '../styles/index.css';
 
 const Letter = ({ char, x0, y0, check }) => {
@@ -13,6 +17,9 @@ const Letter = ({ char, x0, y0, check }) => {
 
   const space = char === ' ';
 
+  // This is only visual play, not really interactive elements.
+  // We might make the letters navigable with a keyboard, but that would probably just be annoying.
+  /* eslint-disable jsx-a11y/click-events-have-key-events */
   return (
     <span
       className={`puzzle ${space ? 'space ' : ''}char${(!space && offset) ? ' offset' : ''}`}
@@ -22,16 +29,16 @@ const Letter = ({ char, x0, y0, check }) => {
       {char}
     </span>
   );
+  /* eslint-enable jsx-a11y/click-events-have-key-events */
+};
+
+function getPosition(i) {
+  const x0 = (Math.random() * -1.5) + ((i % 5) * 1.5) - 1.5;
+  const y0 = (Math.random() * -1.5) + ((i / 5) - 0.5);
+  return { x0, y0 };
 }
 
-  function getPosition(i) {
-    const x0 = (Math.random() * -1.5) + ((i % 5) * 1.5) - 1.5;
-    const y0 = (Math.random() * -1.5) + ((i / 5) - 0.5);
-    return { x0, y0 };
-  }
-
-
-const IndexPage = () => {
+const Main = () => {
 
   const text = 'FHIR Demo 2026';
   const chars = React.useRef(text.split('').map(((c, i) => ({ key: `${i}-${c}`, char: c, check: () => check(i), x0: 0.01, y0: 0.01 }))));
@@ -61,7 +68,7 @@ const IndexPage = () => {
       <h1 className={`${completed ? 'completed ' : ''}puzzle`}>
         {puzzle.map(ch => <Letter {...ch} />)}
       </h1>
-      <p>Welcome to the FHIR Demo 2026 website.</p>
+      <p>Welcome to the FHIR Demo 2026 website!</p>
       <p>
         This year, the Nordic FHIR Demo showcase concentrates on presenting the results of the
         Nordic Health Data Hackathons.
@@ -71,25 +78,126 @@ const IndexPage = () => {
         2025</Link>, remain available as a valuable catalog of FHIR implementations and
         implementers in the Nordics.
       </p>
-      <section id="norway-2025">
-        <h2>Norwegian Health Data Hackathon 2025</h2>
+      <section id="hackathons">
+        <a href="#hackathons"><h1>Nordic Health Data Hackathons</h1></a>
+      <section id="organizers">
         <p>
+          The Nordic Health Data Hackathons is a series of events organized by Nordic HL7
+          affiliates
         </p>
-      </section>
-      <section id="finland">
-        <h2>Finnish Health Data Hackathon 2026</h2>
+        <ul>
+          <li>
+            <a href="https://hl7.dk/">
+              <picture>
+                <img src={hl7DenmarkLogo} alt="HL7 Denmark" />
+              </picture>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.hl7.fi/">
+            <picture className="large">
+              <img src={hl7FinlandLogo} alt="HL7 Finland" />
+            </picture>
+          </a>
+          </li>
+          <li>
+            <a href="https://www.hl7.no/">
+            <picture>
+              <img src={hl7NorwayLogo} alt="HL7 Norway" />
+            </picture>
+          </a>
+          </li>
+          <li>
+            <a href="https://hl7.se/">
+            <picture>
+              <img src={hl7SwedenLogo} alt="HL7 Sweden" />
+            </picture>
+          </a>
+          </li>
+        </ul>
         <p>
+          and made possible by dozens of volunteers!
         </p>
-      </section>
-      <section id="sweden">
-        <h2>Swedish Health Data Hackathon 2026</h2>
-        <p>
-        </p>
+      </section>        
+        <section id="norway-2025">
+          <a href="#norway-2025"><h2>Norwegian FHIR Hackathon 2025</h2></a>
+          <p>
+            The first ever event of the hackathon series took place as an official side event of
+            the <a href="https://ehin.no/2025/">EHiN conference 2025</a>.
+          </p>
+          <p>
+            <a className="call-to-action"
+            href="https://hl7norway.github.io/FHIR-hackathon-2025/currentbuild/index.html#result-summary"
+            >See the results!</a>
+          </p>
+        </section>
+        <section id="finland-2026">
+          <a href="#fniland-2026"><h2>Finnish Health Data Hackathon 2026</h2></a>
+          <p>
+            The second hackathon was connected to both the <a
+            href="https://radicalhealthfestival.messukeskus.com/">Radical Health Festival</a> and
+            the <a href="https://laakaripaivat.messukeskus.com/en/">Finnish Medical
+            Convention</a> (<a href="https://laakaripaivat.fi/"><i lang="fi"
+            >Lääkäripäivät</i></a>), and synchronized with the <a
+            href="https://www.hl7.fi/kokoukset-ja-tapahtumat/hl7-finland-30th-anniversary-symposium/"
+            >30th Anniversary Symposium of HL7 Finland</a>.
+          </p>
+          <p>
+            <a className="call-to-action" href="https://fhir.fi/hackathon/2026/finland/results">See
+            the results!</a>
+          </p>
+        </section>
+        <section id="sweden-2026">
+          <a href="#sweden-2026"><h2>Swedish Health Data Hackathon 2026</h2></a>
+          <p>
+            The third hackathon takes place in Gothenburg, Sweden, connected to the <a
+            href="https://vitalis.nu/">Vitalis</a> conference.
+          </p>
+          <p>The date has been confirmed to be Friday, May 8. Mark your calendar!</p>
+          <p>
+            <a className="call-to-action"
+            href="https://chat.fhir.org/#narrow/channel/194447-nordics/topic/Swedish.20Health.20Data.20Hackathon.20-.20Call.20For.20Proposals/with/575395533"
+            >See the Call for Proposals!</a>
+          </p>
+        </section>
+        <section id="denmark-2026">
+          <a href="#denmark-2026"><h2>Denmark</h2></a>
+          <p>
+            The autumn hackathon will be in Denmark. Tentatively connected to the <a
+            href="https://2025.e-sundhedsobservatoriet.dk/">Danish E-Health Observatory</a> event.
+          </p>
+        </section>
+        <section id="norway-2026">
+          <a href="#norway-2026"><h2>Norwegian FHIR Hackathon 2026</h2></a>
+          <p>
+            On fourth quarter we return to Norway, where the hackathon will be organised as a side
+            event of the <a href="https://ehin.no/">EHiN conference</a>.
+          </p>
+        </section>
       </section>
     </main>
   )
-}
+};
 
-export default IndexPage
+const Footer = () => {
+  return (
+    <footer>
+      <nav>
+        <li>&copy; 2026+ <a href="https://hl7.fi/">HL7 Finland</a></li>
+        <li>
+          <a
+            href="https://github.com/fhir-fi/fhir-fi.github.io/tree/fhir-demo-2026-gatsby/fhir-demo-2026"
+          >
+            Source
+          </a>
+        </li>
+      </nav>
+    </footer>
+  );
+};
 
-export const Head = () => <title>FHIR Demo 2026</title>
+const IndexPage = () => <><Main /><Footer/></>;
+
+export default IndexPage;
+
+export const Head = () => <title>FHIR Demo 2026</title>;
